135c7b1d896e6f99fd0c288cd59ec029d8124ccb
Tory Klingenstein <toryklingenstein@gmail.com>
2019-10-22 10:08:20 -0400

    create p for each letter in hidden word

--- main.js
+++ main.js
@@ -4,12 +4,27 @@ let hiddenWord = "bananas"
 let hiddenArray = hiddenWord.split("");
 console.log(hiddenArray)
 
+// need to create hidden p for each letter in the array
+const main = document.querySelector("main")
+for (let i = 0; i < hiddenArray.length; i++) {
+	main.appendChild(document.createElement("p"))
+	// let hiddenLetters = document.querySelectorAll("p")
+	// for (let j = 0; j < hiddenLetters.length; j++) {
+	// 	hiddenLetters[j].innerText = hiddenArray[i]
+	// }
+	// p.classList.add("hidden")
+}
+let hiddenLetters = document.querySelectorAll("p")
+for (let i = 0; i < hiddenLetters.length; i++) {
+	hiddenLetters[i].innerText = hiddenArray[i]
+	hiddenLetters[i].classList.add("hidden")
+}
 function checkLetter(letter) {
 	for(let i = 0; i < hiddenArray.length; i++) {
 		if (letter === hiddenArray[i]) {
-			console.log("yay")
+			console.log(letter)
 		}
 	}
 }
 
-checkLetter("a")
+// checkLetter("a")
