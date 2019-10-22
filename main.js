fac25466364705d6a6ecac3363e06d0260a5d891
Tory Klingenstein <toryklingenstein@gmail.com>
2019-10-22 10:49:07 -0400

    switch to oop

--- main.js
+++ main.js
@@ -1,30 +1,50 @@
-let hiddenWord = "bananas"
-
-// need to break string into array of letters
-let hiddenArray = hiddenWord.split("");
-console.log(hiddenArray)
-
+// switching to oop
 const main = document.querySelector("main")
-// this creates a p for each letter in the array
-for (let i = 0; i < hiddenArray.length; i++) {
-	main.appendChild(document.createElement("p"))
-}
-// this adds one letter from the hidden word to each p and adds a hidden class
-let hiddenLetters = document.querySelectorAll("p")
-for (let i = 0; i < hiddenLetters.length; i++) {
-	hiddenLetters[i].innerText = hiddenArray[i]
-	hiddenLetters[i].classList.add("hidden")
-}
 
-function checkLetters(letters) {
-	letters.forEach(letter => {
-		for(let i = 0; i < hiddenLetters.length; i++) {
-			if (letter === hiddenLetters[i].innerText) {
-				hiddenLetters[i].classList.remove("hidden")
-			}
+class HiddenWord {
+	constructor(word) {
+		this.array = word.split("")
+	}
+	createLetterDivs() {
+		for (let i = 0; i < this.array.length; i ++) {
+			main.appendChild(document.createElement("p"))
 		}
-	})
+	}
+	addLetterContent() {
+		let hiddenLetters = document.querySelectorAll("p")
+		for (let i = 0; i < this.array.length; i ++) {
+			hiddenLetters[i].innerText = this.array[i]
+			// hiddenLetters[i].classList.add("hidden")
+		}
+	}	
+}
+class Guess {
+	constructor(string) {
+		this.array = string.split("")
+	}
+}
+class Game {
+	constructor(word) {
+		this.score = []
+		this.goalWord = new HiddenWord(word)
+		this.goalWord.createLetterDivs()
+		this.goalWord.addLetterContent()
+	}
+	checkLetters(letters) {
+		letters.forEach(letter => {
+			for(let i = 0; i < hiddenLetters.length; i ++) {
+				if(letter === hiddenLetters[i].innerText) {
+					hiddenLetters[i].classList.remove("hidden")
+				}
+			}
+		})
+	}
 }
 
-let guessArray = ["a", "n", "b", "s"]
-checkLetters(guessArray)
+const game = new Game("potatoes")
+console.log(game.goalWord.array)
+
+
+
+
+
